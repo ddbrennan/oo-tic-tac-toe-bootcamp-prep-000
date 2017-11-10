847f4167d5ff5c9ef7a44728e564c3029cfa54c2
David Brennan <ddbren@gmail.com>
2017-11-10 00:47:21 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -79,9 +79,10 @@ class TicTacToe
     won? || draw?
   end
 
-def winner(board)
-  if winning_combo = won?(board)
-    board[winning_combo.first]
+  def winner
+    if winning_combo = won?
+      @board[winning_combo.first]
+    end
   end
-end
+  
 end
